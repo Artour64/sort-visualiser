e08271970e0ea3e5e5c9aae73363d7520134cbe8
Artour Konstantinov <artoukons@gmail.com>
2024-01-14 17:58:59 -0500

    clean up config with presets

--- config.py
+++ config.py
@@ -1,25 +1,27 @@
-delay=0
+
+preset = 0
+preset = 3
+#preset = 6
+
+presets = [
+	[0,2000,1],#	0
+	[0,1000,2],#	1
+	[0,500,4],#		2
+	[0,200,8],#		3
+	[10,100,16],#	4
+	[20,50,32],#	5
+	[100,20,64]#		6
+]
+
+preset = presets[preset]
+
+delay = preset[0]
+listLen = preset[1]
+tileSize = preset[2]
+
 #delay=10
-#delay=20
-#delay=300
-
-listLen=2000
-#listLen=50
-#listLen=100
-#listLen=101
-#listLen=10
-#listLen=200
-#listLen=500
-#listLen=20
 #listLen=9
-
-tileSize=16;
-tileSize=8;
-#tileSize=4
-tileSize=1
 #tileSize=64
-#tileSize=32
-
 
 vis=True;
 #vis=False;#not working properly
